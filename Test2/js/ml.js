0c50d651349ec8070cf38c9bc700c0a28d534f31
Shrey Patel <patelshrey4201@gmail.com>
2018-11-15 14:49:04 -0500

    Made changes in the code

--- Test2/js/ml.js
+++ Test2/js/ml.js
@@ -24,10 +24,21 @@ function createCanvas(){
   canvas.height = img.height;
   canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
   var colors = [];
-  for ( var i = 0; i < canvas.height; i++; ) {
-    for ( var j = 0; j < canvas.width; j++; ) {
-      colors.push(canvas.getContext('2d').getImageData(j, i, 1, 1).data);
+  for( var i = 0; i < canvas.height; i++ ) {
+    colors.push( [] );
+  }
+  for ( i = 0; i < canvas.height; i++ ) {
+    for ( var j = colors[i].length; j < canvas.width; j++ ) {
+      colors[i].push(canvas.getContext('2d').getImageData(j, i, 1, 1).data);
     }
   }
   console.log(colors);
+  // identifyShapes(colors);
+}
+function identifyShapes( colors ){
+  var backgroundColor;
+  var shapeColor;
+  for ( i = 0; i < canvas.height; i++ ) {
+    // for ( j = 0; j < canvas.width; )
+  }
 }
